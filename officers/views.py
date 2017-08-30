a253e44a5f321a3c4c68dd6f747ae6751b6e4b3f
Jim Heald <james.r.heald@gmail.com>
2017-08-30 16:12:04 -0700

    Got the order right

--- officers/views.py
+++ officers/views.py
@@ -3,9 +3,26 @@ from django.http import HttpResponse
 
 from .models import Officer
 
+officers_order = {
+        "Pres": 0,
+        "Vice": 1,
+        "Mark": 2,
+        "Outr": 3,
+        "Secr": 4,
+        "Trea": 5,
+        "Webm": 6
+}
+
 # Create your views here.
 def index(request):
     officers = Officer.objects.all()
-    context = {'officers_list': officers}
+
+    officers_list = len(officers_order)*[None]
+    for officer in officers:
+        idf = officer.position[:4]
+        pos = officers_order[idf]
+        officers_list[pos] = officer
+
+    context = {'officers_list': officers_list}
     return render(request, 'officers/index.html', context)
 

7661a84d0b1b254b86ff957ebfbcb50dff7f5630
Jim Heald <james.r.heald@gmail.com>
2017-08-29 22:25:56 -0700

    Some fields can be blank

--- officers/models.py
+++ officers/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Officer(models.Model):
     name = models.CharField('Name', max_length=50)
     position = models.CharField('Position', max_length=30)
-    username = models.CharField('Username', max_length=30)
+    username = models.CharField('Username', max_length=30, blank=True)
     email = models.EmailField('Email')
-    website = models.URLField('Website')
-    github = models.URLField('GitHub')
-    linkedin = models.URLField('LinkedIn')
+    website = models.URLField('Website', blank=True)
+    github = models.URLField('GitHub', blank=True)
+    linkedin = models.URLField('LinkedIn', blank=True)
     description = models.CharField('Description', max_length=200)

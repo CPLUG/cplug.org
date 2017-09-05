52cd0e34874a229c0596c87d116cf1638bf5d425
Jim Heald <james.r.heald@gmail.com>
2017-09-04 21:49:25 -0700

    It's going to be /api until everything is under one repo

--- cplugbackend/urls.py
+++ cplugbackend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-endpoint='cplug'
+endpoint = "api"
 
 urlpatterns = [
     url(rf'^{endpoint}/admin/', admin.site.urls),

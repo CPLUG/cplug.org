c3e2e4a87dffacc3be3f61135fdf3a9ccbf794c5
Jim Heald <james.r.heald@gmail.com>
2017-09-28 14:17:58 -0700

    Incorporate our new officer (note: HACKY SOLUTION. Need to keep order in DB)

--- index/views.py
+++ index/views.py
@@ -4,11 +4,12 @@ from .models import Event, Officer
 officers_order = {
         "Pres": 0,
         "Vice": 1,
-        "Mark": 2,
-        "Outr": 3,
-        "Secr": 4,
-        "Trea": 5,
-        "Webm": 6
+        "Comm": 2,
+        "Mark": 3,
+        "Outr": 4,
+        "Secr": 5,
+        "Trea": 6,
+        "Webm": 7
 }
 
 # Create your views here.
